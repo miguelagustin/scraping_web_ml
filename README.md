bdcaead8bc715fdaa985c62320ac5fb8f2706f58
Miguel Agustin <miguelagustin322@gmail.com>
2021-07-17 18:56:53 -0300

    fork genesis

--- README.md
+++ README.md
@@ -1,36 +1,3 @@
 # scraping_web_ml
 
-## Descripcion
-
-Programa con Interfaz Grafica que scrapea paginas web de busquedas de mercadolibre y parsea los items buscados a un archivo csv para poder analizarlo o insertarlo en alguna tabla.  
-Utiliza tkinter como GUI (nativo en Python), beautifulsoup y requests para el scraping.  
-
-Se encuentran dos scripts, la diferencia entre uno y otro es que uno corre con Interfaz Grafica y otro en consola.
-
-## Funcionamiento
-
-El programa funciona de la siguiente manera:
-
-1) Se reciben por parametro el producto a buscar y la cantidad de paginas para visitar en mercadolibre. Por defecto 1.  
-2) Por cada producto de la busqueda se toman los siguientes datos: "producto|precio|url_producto|reviews|estado|cantidad_vendidos"  
-3) Una vez buscado todos los datos se guarda en un archivo csv con el formato "busqueda_{producto}_ml.csv" en la carpeta output del script.
-
-## Parametros
-
-El script de consola recibe los siguientes parametros:
-
-**--busqueda** : Producto de ml a buscar  
-**--paginas** (opcional) : Cantidad de paginas a buscar, por defecto 1.
-
-**Ejemplo:**
-
-    python scraping_mercadolibre_cli.py --busqueda "placas de video nvidia" --paginas 5
-
-## Requerimientos
-
-Necesitan tener los siguientes modulos instalados:  
-
-- **requests**
-- **bs4 (BeautifulSoup4)**  
-
-Para poder instalarlo tienen que abrir su consola cmd/shell y escribir: "pip install requests --user" y "pip install bs4 --user".
+Simple script para obtener alertas de nuevas publicaciones en Mercado Libre
--- scraping_mercadolibre_cli.py
+++ scraping_mercadolibre_cli.py
@@ -3,104 +3,151 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import os
-import time
-import argparse
-
-# Scraping de mercadolibre
-
-# Argumentos
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--busqueda", help="Producto a buscar en ML")
-parser.add_argument("--paginas", help="Cantidad de paginas a buscar (Default: 1)")
-
-args = parser.parse_args()
-
-if args.busqueda:
-	busqueda = args.busqueda.replace(" ", "-")
-else:
-	print("Error: No se ingreso un producto a buscar")
-	sys.exit(1)
-
-if args.paginas:
-	try:
-		count_pages = int(args.paginas)
-	except BaseException as err:
-		print("Error: el parametro ingresado no es un numero")
-		sys.exit(1)
-	if count_pages < 1:
-		print("Error: Las paginas no pueden ser menor a 1")
-		sys.exit(1)
-	else:
-		print(f"Se ingresaron {count_pages} paginas")
-else:
-	print(f"No se ingreso cantidad de paginas, por defecto sera 1")
-	count_pages = 1
-
-# Armado del url mercado libre 
-url = "https://listado.mercadolibre.com.ar/"
-url += busqueda + "_DisplayType_G"
-
-#Cabecera
-file = open(os.path.join("output", "busqueda_" + busqueda + "_ml.csv"),"w", encoding="utf-8-sig")
-file.write("producto|precio|url_producto|reviews|estado|cantidad_vendidos\n")
-file.close()
-
-print("Url: ", url)
-
-#Scraping
-for i in range (0, count_pages):
-	try:
-		pag_web = requests.get(url)
-	except requests.RequestException as e:
-		print("Error al realizar la peticion de la pagina, posiblemente esta caida o no exista " + e)
-		sys.exit(1)
-	soup = BeautifulSoup(pag_web.text,'html.parser')
-	titulo_url = soup.title.string
-	print (titulo_url + " | Pagina " + str(i+1))
-	file = open(os.path.join("output", "busqueda_" + busqueda + "_ml.csv"),"a+", encoding="utf-8-sig")
-	for parte in soup.find_all('a','ui-search-result__content ui-search-link'):
-		# Titulo
-		titulo_ml = parte.find('h2','ui-search-item__title ui-search-item__group__element').string
-		file.write(titulo_ml + "|")
-		# Precio
+from time import sleep
+import re
+import pickle
+import sqlite3
+from pprint import pprint
+import winsound
+class Publicacion:
+	id_regex = re.compile(r'[0-9]{6,}')
+	def __init__(self, titulo, precio, url, search_term):
+		self.titulo = titulo
+		self.precio = precio
+		self.url = url
+		self.search_term = search_term
 		try:
-			precio_ml = (parte.find('div','ui-search-price ui-search-price--size-medium ui-search-item__group__element')).find('span','price-tag-fraction').string.replace(".", "")
-			file.write(precio_ml + "|")
-		except:
-			file.write("|")
-		# Url
-		url_prod = parte.get('href')
-		file.write(url_prod + "|")
-		# Reviews
+			self.id = self.id_regex.findall(self.url)[0]
+		except IndexError:
+			self.id = None
+	def __str__(self):
+		return '{} | {}'.format(self.titulo, self.precio)
+	def __repr__(self):
+		return '{} | {}'.format(self.titulo, self.precio)
+	def __hash__(self):
+		return hash(self.id)
+	def __eq__(self, other):
+		return self.id == other.id
+
+
+
+def get_publicaciones(busqueda, count_pages):
+	publicaciones = []
+	url = "https://listado.mercadolibre.com.ar/"
+	url += busqueda + "_DisplayType_G"
+	for i in range (0, count_pages):
 		try:
-			reviews = parte.find('span','ui-search-reviews__amount').string
-			file.write(reviews + "|")
-		except:
-			file.write("|")
-		# Unidades Vendidas / Estado
-		soup_prod = BeautifulSoup(requests.get(url_prod).text,'html.parser')
+			pag_web = requests.get(url)
+		except requests.RequestException as e:
+			print("Error al realizar la peticion de la pagina, posiblemente esta caida o no exista " + e)
+			sys.exit(1)
+		soup = BeautifulSoup(pag_web.text,'html.parser')
+		# titulo_url = soup.title.string
+		# print (titulo_url + " | Pagina " + str(i+1))
+		for parte in soup.find_all('a','ui-search-result__content ui-search-link'):
+			# Titulo
+			titulo_ml = parte.find('h2','ui-search-item__title ui-search-item__group__element').string
+			# file.write(titulo_ml + "|")
+			# Precio
+			try:
+				precio_ml = (parte.find('div','ui-search-price ui-search-price--size-medium ui-search-item__group__element')).find('span','price-tag-fraction').string.replace(".", "")
+				# file.write(precio_ml + "|")
+			except:
+				continue
+			# Url
+			url_prod = parte.get('href')
+			if url_prod.startswith("https://click1"):
+				continue
+			publicaciones.append(Publicacion(titulo_ml, precio_ml, url_prod, busqueda))
 		try:
-			subtexto = soup_prod.find("span", "ui-pdp-subtitle").string.split("|")
-			estado = subtexto[0].strip()
-			if len(subtexto) == 2:
-				cant_vendidos = subtexto[1].replace("vendido", "").replace("s", "").strip()
-			else:
-				cant_vendidos = "0"
-			file.write(estado + "|")
-			file.write(cant_vendidos)
-		except:
-			file.write("|")
-		# Fin de registro
-		file.write("\n")
-	file.close()
+			link_sig = (soup.find('li','andes-pagination__button andes-pagination__button--next')).find('a','andes-pagination__link')
+		except AttributeError:
+			break
+		url = link_sig.get('href')
+	return publicaciones
+
+#function to create sqlite database of publicaciones with a foreign key to another table called 'query'
+def create_database():
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
+	c.execute('''CREATE TABLE query
+		(search_term varchar(255) PRIMARY KEY,
+		tracked INTEGER)''')
+	conn.commit()
+	c.execute('''CREATE TABLE publicaciones
+		(id INTEGER PRIMARY KEY,
+		titulo varchar(255),
+		precio INTEGER,
+		url varchar(255),
+		search_term varchar(255),
+		FOREIGN KEY(search_term) REFERENCES query(search_term))''')
+	conn.commit()
+	conn.close()
+
+
+#function to save publicaciones to the database
+def save_publicaciones(publicaciones):
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
 	try:
-		link_sig = (soup.find('li','andes-pagination__button andes-pagination__button--next')).find('a','andes-pagination__link')
-	except Exception as e:
-		print("Error al encontrar el siguiente link. " + str(e))
-		sys.exit(1)
-	url = link_sig.get('href')
+		c.execute("INSERT INTO query VALUES(?,?)", (publicaciones[0].search_term, 1))
+	except sqlite3.IntegrityError:
+		pass
+	for publicacion in publicaciones:
+		c.execute("INSERT or REPLACE INTO publicaciones VALUES(?,?,?,?,?)", (publicacion.id, publicacion.titulo, publicacion.precio, publicacion.url, publicacion.search_term))
+		conn.commit()
+	conn.close()
 
-print("Fin del proceso")
 
-sys.exit(0)
+#function to retrieve publicaciones from database with a given query
+def load_publicaciones(query):
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM publicaciones WHERE search_term = ?", (query,))
+	publicaciones = c.fetchall()
+	publicaciones = [Publicacion(x[1], x[2], x[3], x[4]) for x in publicaciones]
+	conn.commit()
+	conn.close()
+	return publicaciones
+
+def load_tracked_queries():
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
+	c.execute("SELECT * FROM query WHERE tracked = 1")
+	queries = [x[0] for x in c.fetchall()]
+	conn.commit()
+	conn.close()
+	return queries
+
+def change_tracked_status(query, status):
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
+	c.execute("UPDATE query SET tracked = ? WHERE search_term = ?", (status, query))
+	conn.commit()
+	conn.close()
+
+def save_query(query):
+	conn = sqlite3.connect('publicaciones.db')
+	c = conn.cursor()
+	try:
+		c.execute("INSERT INTO query VALUES(?,?)", (query, 1))
+	except sqlite3.IntegrityError:
+		pass
+	conn.commit()
+	conn.close()
+
+
+if __name__ == '__main__':
+	if not os.path.isfile('publicaciones.db'):
+		create_database()
+	queries_to_watch = load_tracked_queries()
+	while True:
+		for query in queries_to_watch:
+			publicaciones = get_publicaciones(query, 100)
+			old_publicaciones = load_publicaciones(query)
+			new_publicaciones = set(publicaciones) - set(old_publicaciones)
+			save_publicaciones(publicaciones)
+			if new_publicaciones:
+				pprint(new_publicaciones)
+				winsound.PlaySound('notification_sound.wav', winsound.SND_FILENAME)
+			sleep(60)
